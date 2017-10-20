5b76999cfab1780e5aa1f86b68a936a5cd07f635
David Warren <warrend2000@gmail.com>
2017-10-20 09:45:53 -0500

    Fixed fullscreen exit on scene load
    Previously, every time SceneManager.LoadScene was called, fullscreen would be exited. That's been fixed :)

--- Assets/Scripts/Device/ScreenResolution.cs
+++ Assets/Scripts/Device/ScreenResolution.cs
@@ -5,16 +5,16 @@
 /// Mostly here to prevent high refresh rate screens from being unable to play the game as a lot of scripts are tied to per-frame Update loops.
 /// </summary>
 public class ScreenResolution : MonoBehaviour {
-    public static bool hasInitialized = false;
+    //public static bool hasInitialized = false;
 
     private void Start() {
-        if (hasInitialized) {
+        /*if (hasInitialized) {
             Destroy(this);
             return;
-        }
+        }*/
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
-        Screen.SetResolution(640, 480, false, 0);
-        hasInitialized = true;
+		Screen.SetResolution(640, 480, Screen.fullScreen, 0);
+        //hasInitialized = true;
     }
 }
