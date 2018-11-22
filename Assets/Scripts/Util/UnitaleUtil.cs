RhenaudTheLukark/CreateYourFrisk

commit 87a28536ef4d8f1d647163dff11dae137face3c4

Fixed SetText infinite loop with unmatched ]
* Fixed a major bug where an infinite loop would occur and crash CYF if
a lua text object called SetText, using text with an opening [ without a
matching ].

Also fixed a case that looked very similar in LuaScriptBinder.cs

This fixes issue
https://github.com/RhenaudTheLukark/CreateYourFrisk/issues/35

Assets/Scripts/Util/UnitaleUtil.cs
@@ -150,7 +150,6 @@ public static class UnitaleUtil {
                         }
                         str += txtmgr.textQueue[txtmgr.currentLine].Text[j];
                     }
-                    i--;
                     if (str.Split(':')[0] == "charspacing")
                         hSpacing = ParseUtil.GetFloat(str.Split(':')[1]);
                     break;

Assets/Scripts/Lua/LuaScriptBinder.cs
@@ -407,7 +407,7 @@ public static class LuaScriptBinder {
         //////////////////////////////////////////
         
         // If the first line of text has [font] at the beginning, use it intially!
-        if (firstLine.IndexOf("[font:") > -1) {
+        if (firstLine.IndexOf("[font:") > -1 && firstLine.IndexOf(']') > firstLine.IndexOf("[font:")) {
             // grab all of the text that comes before the matched command
             string precedingText = firstLine.Substring(0, firstLine.IndexOf("[font:"));
             

commit 4f83fb9343107dc63beace080f61a0f3ec5ac54d

Updated fix for infinite loop in text
My previous fix for this, 87a2853, caused the game over text to break
inexplicably. It seems that `i--;` line really is needed...

Assets/Scripts/Util/UnitaleUtil.cs
@@ -143,13 +143,26 @@ public static class UnitaleUtil {
             switch (txtmgr.textQueue[txtmgr.currentLine].Text[i]) {
                 case '[':
                     string str = "";
+                    bool failSafe = false;
                     for (int j = i + 1; j < txtmgr.textQueue[txtmgr.currentLine].Text.Length; j++) {
                         if (txtmgr.textQueue[txtmgr.currentLine].Text[j] == ']') {
                             i = j + 1;
                             break;
                         }
                         str += txtmgr.textQueue[txtmgr.currentLine].Text[j];
+                        
+                        // unclosed [ has been detected
+                        if (j == txtmgr.textQueue[txtmgr.currentLine].Text.Length - 1) {
+                            failSafe = true;
+                            break;
+                        }
                     }
+                    
+                    // used to protect against unclosed open brackets
+                    if (failSafe)
+                        break;
+                    
+                    i--;
                     if (str.Split(':')[0] == "charspacing")
                         hSpacing = ParseUtil.GetFloat(str.Split(':')[1]);
                     break;