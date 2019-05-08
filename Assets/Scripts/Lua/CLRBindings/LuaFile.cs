fc1d3c4cb777e2ba6d574b4f4d35272b2964e99e
WD <warrend2000@gmail.com>
2019-05-07 20:28:45 -0500

    Few changes to the File object
    * Edited some error message text
    
    * Changed the automatic `/` character to a `\\`
    
    * New error for if the given path is invalid (goes through a folder that doesn't exist)
    
    * Added `File.openMode` as a read-only variable

--- Assets/Scripts/Lua/CLRBindings/LuaFile.cs
+++ Assets/Scripts/Lua/CLRBindings/LuaFile.cs
@@ -11,21 +11,23 @@ public class LuaFile {
         get { return content.Length; }
     }
 
-    public string fileMode {
+    public string openMode {
         get { return mode; }
     }
 
     public LuaFile(string path, string mode = "rw") {
         if (path.Contains(".."))
-            throw new CYFException("You cannot open a file outside of the mod. The use of \"..\" allows the modder to reach beyond what it is supposed to reach, so it has been forbidden entirely.");
+            throw new CYFException("You cannot open a file outside of a mod folder. The use of \"..\" is forbidden.");
         if (path == null)
-            throw new CYFException("The path of the file can't be nil.");
-        path = FileLoader.ModDataPath + "/" + path;
+            throw new CYFException("Attempt to open a file with a nil path.");
+        path = FileLoader.ModDataPath + "\\" + path;
 
         if (mode != "r" && mode != "w" && mode != "rw" && mode != "wr")
             throw new CYFException("A file's open mode can only be r (read), w (write) or rw (read + write).");
         if (mode == "r" && !File.Exists(path))
             throw new CYFException("You can't open a file that doesn't exist in read-only mode.");
+        if (!Directory.Exists(path.Substring(0, path.Length - Path.GetFileName(path).Length)))
+            throw new CYFException("Invalid path:\n\n\"" + path + "\"");
 
         this.path = path;
         this.mode = mode;
@@ -39,7 +41,7 @@ public class LuaFile {
         if (!File.Exists(path))
             throw new CYFException("The file at the path \"" + path + "\" doesn't exist, so you can't read from it.");
         if (line > content.Length || line < 1 || line % 1 != 0)
-            throw new CYFException("The file only has " + content.Length + " lines yet you're trying to access this file's line #" + line);
+            throw new CYFException("Cannot read line #" + line + " of a file with " + content.Length + " lines.");
         return content[line - 1];
     }
 
@@ -53,9 +55,9 @@ public class LuaFile {
 
     public void Write(string data, bool append = true) {
         if (!mode.Contains("w"))
-            throw new CYFException("This file has been opened in read-only mode, you can't write anything in it.");
+            throw new CYFException("This file has been opened in read-only mode, you can't write anything to it.");
         if (data == null)
-            throw new CYFException("You can't write nil in a file! If you want to empty the file, use an empty string with the append parameter set to false instead.");
+            throw new CYFException("You can't write nil to a file! If you want to empty the file, use an empty string with the append parameter set to false instead.");
 
         if (!File.Exists(path))
             File.Create(path).Close();
@@ -68,9 +70,9 @@ public class LuaFile {
     
     public void ReplaceLine(int line, string data) {
         if (!mode.Contains("w"))
-            throw new CYFException("This file has been opened in read-only mode, you can't write anything in it.");
+            throw new CYFException("This file has been opened in read-only mode, you can't write anything to it.");
         if (line > content.Length || line < 1 || line % 1 != 0)
-            throw new CYFException("The file only has " + content.Length + " lines yet you're trying to replace this file's line #" + line);
+            throw new CYFException("Cannot replace line #" + line + " of a file with " + content.Length + " lines.");
         if (data == null)
             throw new CYFException("You can't set a line to nil! If you want to remove the line, use the function DeleteLine().");
 
@@ -93,7 +95,7 @@ public class LuaFile {
 
     public void DeleteLine(int line) {
         if (!mode.Contains("w"))
-            throw new CYFException("This file has been opened in read-only mode, you can't write anything in it.");
+            throw new CYFException("This file has been opened in read-only mode, you can't write anything to it.");
         if (line > content.Length || line < 1 || line % 1 != 0)
             throw new CYFException("The file only has " + content.Length + " lines yet you're trying to delete this file's line #" + line);
 
@@ -105,7 +107,7 @@ public class LuaFile {
 
     public void Delete() {
         if (!mode.Contains("w"))
-            throw new CYFException("This file has been opened in read-only mode, you can't write anything in it.");
+            throw new CYFException("This file has been opened in read-only mode, you can't write anything to it.");
         if (File.Exists(path))
             File.Delete(path);
     }
