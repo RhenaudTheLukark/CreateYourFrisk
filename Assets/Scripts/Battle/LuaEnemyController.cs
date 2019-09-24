1bf79e30d845559bb3bcbd55f4371f20e894c684
Bafadem Benjamin <benjamin_bafadem@ens.univ-artois.fr>
2019-09-24 15:24:44 +0200

    Added the possibility of changing the miss texts

--- Assets/Scripts/Battle/LuaEnemyController.cs
+++ Assets/Scripts/Battle/LuaEnemyController.cs
@@ -128,6 +128,14 @@ public class LuaEnemyController : EnemyController {
         get { return (int)script.GetVar("gold").Number; }
         set { script.SetVar("gold", DynValue.NewNumber(value)); }
     }
+    public string DefenseMissText {
+        get { return script.GetVar("defensemisstext").String; }
+        set { script.SetVar("defensemisstext", DynValue.NewString(value)); }
+    }
+    public string NoAttackMissText {
+        get { return script.GetVar("noattackmisstext").String; }
+        set { script.SetVar("noattackmisstext", DynValue.NewString(value)); }
+    }
 
     public override string DialogBubble {
         get {
