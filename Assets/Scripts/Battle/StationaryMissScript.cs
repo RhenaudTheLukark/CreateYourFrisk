3b049750a1a541b786b75d87455586e5c242dc10
Rhenaud Dubois <rhen33390@gmail.com>
2019-09-25 21:20:12 +0200

    Revert "MISS text configuration (#62)"
    This reverts commit d860026da8d7855eae834c7a765f1b92b5fdb5a7.

--- Assets/Scripts/Battle/StationaryMissScript.cs
+++ Assets/Scripts/Battle/StationaryMissScript.cs
@@ -6,20 +6,15 @@
 public class StationaryMissScript : MonoBehaviour {
     private float secondsToDespawn = 1.5f;
     private float despawnTimer = 0.0f;
-    public string text = "MISS";
 
     public void setXPosition(float xpos) {
         GetComponent<RectTransform>().position = new Vector2(xpos - 55, 430); // 55 is the the fairly static 1/2 width of the miss text
     }
 
-    public void SetText(string _text) {
-        text = _text;
-    }
-
 	void Start () {
         TextManager mgr = GetComponent<TextManager>();
         mgr.SetFont(SpriteFontRegistry.Get(SpriteFontRegistry.UI_DAMAGETEXT_NAME));
-        mgr.SetText(new TextMessage("[color:c0c0c0]" + text, false, true));
+        mgr.SetText(new TextMessage("[color:c0c0c0]MISS", false, true));
 	}
 
     void Update(){
