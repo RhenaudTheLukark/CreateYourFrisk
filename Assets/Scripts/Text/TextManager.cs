RhenaudTheLukark/CreateYourFrisk

commit 6c53959f822f0efc72f33e94893175935c69b40a

Fix \t position being dependent on camera position
* Fixed a bug where the second column of the ACT and ITEM menu (technically the \t character) had its x position altered by Misc object properties that move the camera

Assets/Scripts/Text/TextManager.cs
@@ -698,7 +698,7 @@ public class TextManager : MonoBehaviour {
                     currentY = currentY - vSpacing - Charset.LineSpacing;
                     break;
                 case '\t':
-                    currentX = 356 + Camera.main.transform.position.x - 320; // HACK: bad tab usage
+                    currentX = 356; // HACK: bad tab usage
                     break;
                 case ' ':
                     if (i + 1 == currentText.Length)

commit 5f0f384c33dcd6df225c30ea2b4f2f60967c99c6

continue 6c53959
fixed bug introduced by 6c53959 making \t work in battles but not in the overworld. now it works well in both

continues 6c53959

Assets/Scripts/Text/TextManager.cs
@@ -658,7 +658,7 @@ public class TextManager : MonoBehaviour {
                     currentY = currentY - vSpacing - Charset.LineSpacing;
                     break;
                 case '\t':
-                    currentX = 356; // HACK: bad tab usage
+                    currentX = !GlobalControls.isInFight ? (356 + Camera.main.transform.position.x - 320) : 356; // HACK: bad tab usage
                     break;
                 case ' ':
                     if (i + 1 == currentText.Length || currentText[i + 1] == ' ')