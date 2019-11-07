4068e0f2df1bd4fc13edad04449446be409ca308
WD200019 <warrend2000@gmail.com>
2019-11-07 15:31:38 -0600

    DisplayText changes and Stare5

--- Assets/Mods/Examples/Lua/Events/StareTest.lua
+++ Assets/Mods/Examples/Lua/Events/StareTest.lua
@@ -7,24 +7,44 @@ currEventDone = false
 inputted = false
 maxStares = 8
 
-function DisplayText(text, faceSprite)
+displayTextOneFrameMovement = false
+function DisplayText(text, formatted, faceSprites, side)
+    local textStr = "{"
     if type(text) == "table" then
         for i = 1, #text do
-            SetGlobal("CYFOWStareText" .. i, text[i])
+            textStr = textStr .. "'" .. tostring(text[i]):gsub("\n", "\\\\n"):gsub("'", "\\'") .. "'" .. (next(text, i) and ", " or "")
         end
     else
-        SetGlobal("CYFOWStareText1", text)
+        textStr = textStr .. "'" .. tostring(text):gsub("\n", "\\\\n"):gsub("'", "\\'") .. "'"
+    end
+    textStr = textStr .. "}"
+
+    local faceStr
+    if type(faceSprites) == "table" then
+        faceStr = "{"
+        for i = 1, #faceSprites do
+            if type(faceSprites[i]) == "table" then
+                faceStr = faceStr .. "{"
+                for j = 1, #faceSprites[i] do
+                    faceStr = faceStr .. (type(faceSprites[i][j]) == "string" and "'" or "") .. faceSprites[i][j] .. (type(faceSprites[i][j]) == "string" and "'" or "") .. (next(faceSprites[i], j) and ", " or "")
+                end
+                faceStr = faceStr .. "}"
+            else
+                faceStr = faceStr .. "'" .. faceSprites[i] .. "'" .. (next(faceSprites, i) and ", " or "")
+            end
+        end
+        faceStr = faceStr .. "}"
+    else
+        faceStr = "'" .. faceSprites .. "'"
     end
 
-    if type(faceSprite) == "table" then
-        for i = 1, #faceSprite do
-            SetGlobal("CYFOWStareFace" .. i, faceSprite[i])
-        end
-    else
-        SetGlobal("CYFOWStareFace1", faceSprite)
-    end
+    SetGlobal("CYFOWStareSetDialog1", textStr)
+    SetGlobal("CYFOWStareSetDialog2", formatted)
+    SetGlobal("CYFOWStareSetDialog3", faceStr)
+    SetGlobal("CYFOWStareSetDialog4", side)
 
     Event.SetPage("Event1", 4)
+    displayTextOneFrameMovement = true
 end
 
 function resetStareVars()
@@ -38,6 +58,9 @@ function resetStareVars()
     stare3Count = 0
     stare3DogSpeed = 0
     stare4Count = 0
+    stare5Count = 0
+    stare5Phase = 0
+    stare5Velocity = 0
 end
 
 punderSprite = nil
@@ -304,7 +327,192 @@ function Stare4(frame) -- requires at least 574 frames
     stare4Count = mk and stare4Count + 1 or stare4Count
 end
 
-function Stare5(frame) DEBUG("Stare5: " .. frame) return true end
+function Stare5(frame)
+    -- Create Papyrus sprite
+    if frame == 0 and not inputted then
+        papy = CreateSprite("Overworld/Papyrus/0")
+        papy.z = -1
+        papy.Scale(2, 2)
+        papy.ypivot = 0
+        papy.MoveToAbs(-papy.width/2, 230)
+        papy.SetAnimation({"0", "1", "0", "2"}, 0.1875, "Overworld/Papyrus")
+    end
+
+    if not inputted or (inputted and stare5Phase > 7) then
+        -- Walk right #1
+        if stare5Phase == 0 and papy.x < 260 then
+            papy.x = math.min(papy.x + 2, 260)
+
+            if papy.x == 260 then
+                papy.animationspeed = 0.2
+                stare5Phase = 1
+            end
+        -- Walk right #2 (slower)
+        elseif stare5Phase == 1 and papy.x < 372 then
+            papy.x = math.min(papy.x + 1, 372)
+            papy.y = papy.y - 0.5
+
+            if papy.x == 372 then
+                papy.StopAnimation()
+                papy.Set("Overworld/Papyrus/0")
+                stare5Phase = 2
+            end
+        -- Dialogue #1
+        elseif stare5Count ==  310 then
+            stare5Phase = 3
+            papy.SetAnimation({"6", "0"}, 0.2, "Overworld/Papyrus")
+            DisplayText("[noskip][font:papyOW][voice:v_papyrus]HELLO,[w:5] HUMAN![w:10]\nI'VE COME TO SEE WHAT YOU'RE[waitall:5]...[waitall:1][w:20][next]", false, {{"Papyrus/normalT", "Papyrus/normal", 0.2}})
+        elseif stare5Count ==  500 then
+            papy.StopAnimation()
+            papy.Set("Overworld/Papyrus/0")
+        -- Dialogue #2
+        elseif stare5Count ==  700 then
+            stare5Phase = 4
+            papy.SetAnimation({"7", "3"}, 0.2, "Overworld/Papyrus")
+            DisplayText("[noskip][font:papyOW][voice:v_papyrus]HEY!![w:10] ARE YOU LISTENING???[w:20][next]", false, {{"Papyrus/madT", "Papyrus/mad", 0.2}})
+        elseif stare5Count ==  820 then
+            papy.StopAnimation()
+            papy.Set("Overworld/Papyrus/3")
+        -- Dialogue #3
+        elseif stare5Count == 1000 then
+            stare5Phase = 5
+            papy.SetAnimation({"8", "9", "10"}, 0.15, "Overworld/Papyrus")
+            DisplayText("[noskip][font:papyOW][voice:v_papyrus]AAARRGH!!![w:20]\nSTOP IGNORING MEEE!!!!![w:20][next]", false, {{"Papyrus/madT", "Papyrus/mad", 0.2}})
+        elseif stare5Count == 1200 then
+            papy.StopAnimation()
+            papy.Set("Overworld/Papyrus/11")
+        -- Dialogue #4
+        elseif stare5Count == 1400 then
+            stare5Phase = 6
+            papy.SetAnimation({"11", "14"}, 0.2, "Overworld/Papyrus")
+            DisplayText("[noskip][font:papyOW][voice:v_papyrus]Music kept = false\nErm[waitall:3]...[waitall:1][w:10] Why did you go here with your " .. Player.GetHP() .. " HP?[w:30][next]", false, {{"Papyrus/suspiciousT", "Papyrus/suspicious", 0.2}})
+        elseif stare5Count == 1660 then
+            papy.StopAnimation()
+            papy.Set("Overworld/Papyrus/11")
+        -- Walk left
+        elseif stare5Count == 1950 then
+            stare5Phase = 7
+            papy.SetAnimation({"11", "12", "11", "13"}, 0.1875, "Overworld/Papyrus")
+        elseif stare5Phase == 7 and papy.x > 170 then
+            papy.x = math.max(papy.x - 1.5, 170)
+
+            if papy.x == 170 then
+                papy.StopAnimation()
+                papy.Set("Overworld/Papyrus/11")
+                stare5Phase = 8
+            end
+        -- Jump off
+        elseif stare5Count == 2160 then
+            papy.Set("Overworld/Papyrus/18")
+            stare5Phase = 9
+            stare5Velocity = 4
+            -- TODO: play sound?
+        elseif stare5Phase == 9 and stare5Count <= 2360 then
+            papy.x = papy.x - 0.25
+            papy.y = papy.y + stare5Velocity
+            stare5Velocity = stare5Velocity - 0.2
+            papy.rotation = papy.rotation + 0.25
+            
+            -- screm
+            if stare5Count == 2210 then
+                DisplayText("[noskip][font:papyOW][voice:v_papyrus][effect:shake,5]AAAAAAAA[w:10][next]", false, "Papyrus/papy he do a jump")
+            elseif stare5Count == 2360 then
+                papy.Remove()
+            end
+        elseif stare5Count > 2360 then
+            return true
+        end
+    else
+        -- Initial walks right - left half
+        if stare5Phase == 0 or (stare5Phase == 1 and papy.x <= 320) then
+            -- Walk to x=320, then straight up, then remove and end
+            if papy.x < 320 then
+                papy.x = math.min(papy.x + 2, 320)
+
+                -- Begin walking up
+                if papy.x == 320 then
+                    papy.SetAnimation({"15", "16", "15", "17"}, 0.1875, "Overworld/Papyrus")
+                end
+            elseif papy.y < 480 then
+                papy.y = math.min(papy.y + 2, 480)
+
+                -- Remove and end event
+                if papy.y == 480 then
+                    papy.Remove()
+                    return true
+                end
+            end
+        -- Initial walks right - right half
+        elseif stare5Phase == 1 and papy.x > 320 then
+            -- Run once
+            if not papy["walkLeftBool"] then
+                papy["walkLeftBool"] = true
+                papy.SetAnimation({"11", "12", "11", "13"}, 0.1875, "Overworld/Papyrus")
+            end
+
+            papy.x = math.max(papy.x - 2, 320)
+
+            -- As soon as he gets to x=320, the prevoius conditional block will take over and make him walk up
+            if papy.x == 320 then
+                papy.SetAnimation({"15", "16", "15", "17"}, 0.1875, "Overworld/Papyrus")
+            end
+        -- Interrupt speech and flee going South
+        elseif stare5Phase > 1 and stare5Phase < 7 then
+            -- Frame #1
+            if not papy["interrupt"] then
+                papy["interrupt"] = true
+                -- Stop dialogue if any is active
+                if GetGlobal("CYFOWStareSetDialogActive") then
+                    General.EndDialog()
+                end
+            -- Frame #2 (2-frame wait is necessary just for starting the next dialogue)
+            elseif not papy["startflee"] then
+                papy["startflee"] = true
+                papy.SetAnimation({"11", "12", "11", "13"}, 0.1, "Overworld/Papyrus")
+
+                Audio.PlaySound("hitsound", 0.75)
+                Misc.ShakeScreen(8, 8)
+                DisplayText("[noskip][font:papyOW][voice:v_papyrus][speed:3][lettereffect:shake,3]I JUST REMEMBERED[lettereffect:none] I HAVE TO OF IN COLD BONE OF OUT DOG BONE EAT THE BONE[w:10][next]", false, "Papyrus/papy he do a jump")
+            -- Frame #3 onwards
+            else
+                -- Run left
+                if papy.x > 320 then
+                    papy.x = math.max(papy.x - 6, 320)
+
+                    if papy.x == 320 then
+                        papy.SetAnimation({"19", "20", "19", "21"}, 0.1, "Overworld/Papyrus")
+                    end
+                -- Run down
+                elseif (papy.y + (papy.yscale * papy.height)) > 0 or GetGlobal("CYFOWStareSetDialogActive") then
+                    papy.y = papy.y - 6
+                -- Remove and end event
+                else
+                    Audio.PlaySound("runaway", 1)
+                    papy.Remove()
+                    return true
+                end
+            end
+        -- Walking left
+        elseif stare5Phase == 7 then
+            -- Keep walking towards x=170
+            if papy.x > 170 then
+                papy.x = math.max(papy.x - 1.5, 170)
+            -- Walk off screen towards initial spawning position
+            elseif papy.x ~= -papy.width or papy.y ~= 230 then
+                papy.x = math.max(papy.x - 1.5, -papy.width)
+                papy.y = math.min(papy.y + 1.5, 230)
+            -- Remove and end event
+            else
+                papy.Remove()
+                return true
+            end
+        -- Jumping off - at this point it's too late to stop him. So no condition here. RIP papy ;w;
+        end
+    end
+
+    stare5Count = papy and stare5Count + 1 or stare5Count
+end
+
 function Stare6(frame) DEBUG("Stare6: " .. frame) return true end
 function Stare7(frame) DEBUG("Stare7: " .. frame) return true end
 function Stare8(frame) DEBUG("Stare8: " .. frame) return true end
@@ -342,7 +550,12 @@ function EventPage3()
         return
     end
 
-    Player.CanMove(false)
+    if not displayTextOneFrameMovement then
+        Player.CanMove(false)
+    else
+        Player.CanMove(true)
+        displayTextOneFrameMovement = false
+    end
     if not inputted then
         stareFrame = stareFrame + 1
     end
