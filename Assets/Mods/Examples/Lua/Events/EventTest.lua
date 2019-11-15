325598c456e74540b4cd0a8187863a93eb564063
WD200019 <warrend2000@gmail.com>
2019-11-15 00:41:29 -0600

    implement dog event for Asriel
    oh geez oh gosh I really wonder about if we'll have to do anything here for copyrighted sounds oh my goshy

--- Assets/Mods/Examples/Lua/Events/EventTest.lua
+++ Assets/Mods/Examples/Lua/Events/EventTest.lua
@@ -49,7 +49,7 @@ function EventPage10()
     --W 4: MK comes from bottom, looks at player for some time, then goes up
     --W 5: Papyrus come from left, walks to Player, talks to him (standard Pap lines), then gets upset (stomp foot), "Music kept", backs out, then run to left and jump from cliff using window jump
     --R 6: Chara chasing Booster, run in circles, go down, come from top, go down, Chara coming back alone from top, laugh.wav, go left
-    --R 7: Punderbolt & Asriel talk then play tag, then Asriel goes / Asriel enters room, look for something, sit & cry, then wipe tears & go
+    --W 7: Punderbolt & Asriel talk then play tag, then Asriel goes / Asriel enters room, look for something, sit & cry, then wipe tears & go
     --W 8: Chara creepily approaching the player from behind, music slowly fades out, (play anticipation slow mo like in genocide?), then when Chara close to player, hug
     General.SetDialog({ "There's a dog here and it's blocking the way.",
                         "It doesn't look like it'll move any time soon...",
@@ -89,15 +89,11 @@ function EventPage2()
     spr.SetPivot(1, 0)
     spr.Move(16, -8)
     
-    local dogSprite = Event.GetSprite("Event1")
+    local dogSprite = Event.GetSprite(Event.GetName())
     Event.MoveToPoint("Player", 398, 174, true, false)
     spr.Set("MonsterKidOW/f2")
     Audio.PlaySound("Surprised Bark", 1)
     dogSprite.Set("Overworld/DogBark")
-    
-    function lerp(a, b, t)
-        return a + ((b - a) * t)
-    end
     
     local startX = spr.x
     local finalX = spr.x + 16
@@ -192,13 +188,105 @@ function EventPage69()
     -- Actually works + Super Paper Mario spin
     General.SetDialog({ "[voice:v_asriel]There's a dog here and it's blocking the way.",
                         "[voice:v_asriel]It doesn't look like it'll move any time soon...",
-                        "[voice:v_asriel]Hmmm... Maybe I could try being nice to it...?",
-                        "[voice:v_asriel]Mister doggy, may you please let me through?" }, true,
+                        "[voice:v_asriel]Hmmm...[w:20]Maybe I could try being nice to it...?",
+                        "[voice:v_asriel]Mister doggy,[w:10] may you please let me through?" }, true,
                       { { "Asriel/normalT", "Asriel/normal", 0.2 },
                         { "Asriel/sadT",    "Asriel/sad",    0.2 },
                         { "Asriel/normalT", "Asriel/normal", 0.2 },
                         { "Asriel/happyT",  "Asriel/happy" , 0.2 } })
-    Event.SetPage(Event.GetName(), 1)
+    General.Wait(160)
+    
+    -- create cursor sprite
+    Audio.PlaySound("SE1_EVT_LINE_DRAW1")
+    cursor = CreateSprite("Overworld/cursor")
+    cursor.z = -1
+    cursor.SetPivot(0, 1)
+    local dogSprite = Event.GetSprite(Event.GetName())
+    cursor.MoveToAbs(dogSprite.x - (dogSprite.width/2), dogSprite.y + dogSprite.height)
+    cursor.alpha = 0
+    
+    -- create box edges
+    box = {
+        CreateSprite("UI/sq_white"), -- top line
+        CreateSprite("UI/sq_white"), -- left line
+        CreateSprite("UI/sq_white"), -- right line
+        CreateSprite("UI/sq_white")  -- bottom line
+    }
+    
+    for i = 1, #box do
+        box[i].Scale(1/4, 1/4)
+        box[i].SetPivot(0, 1)
+        box[i].MoveTo(cursor.x, cursor.y)
+        box[i].color = {0, 0, 0}
+    end
+    
+    -- animaaate!
+    for timer = 1, 120 do
+        -- cursor
+        if timer <= 10 then
+            cursor.alpha = cursor.alpha + (1/10)
+        elseif timer >= 40 and timer < 80 then
+            cursor.alpha = cursor.alpha - (1/5)
+        end
+        
+        -- move and draw box
+        if timer > 10 and timer < 40 then
+            -- move cursor
+            cursor.x = lerp(cursor.x, dogSprite.absx + (dogSprite.width / 2), 1/6)
+            cursor.y = lerp(cursor.y, dogSprite.absy                        , 1/6)
+            
+            -- draw box
+            box[1].xscale = (cursor.absx - (dogSprite.absx - (dogSprite.width / 2))) /  4
+            box[2].yscale = (cursor.absy - (dogSprite.absy +  dogSprite.height    )) / -4
+            box[3].yscale = box[2].yscale
+            box[3].absx = cursor.absx
+            box[4].xscale = box[1].xscale
+            box[4].absy = cursor.absy
+        -- begin spinny animation
+        elseif timer == 40 then
+            Audio.PlaySound("SE1_EVT_LINE_TURN2")
+            
+            box[1].xpivot = 0.5
+            box[1].x = box[1].x + (box[1].width * box[1].xscale) / 2
+            box[4].SetParent(box[2])
+            box[4].xpivot = 0.5
+            box[4].x = box[4].x + (box[4].width * box[4].xscale) / 2
+            box[2].SetParent(box[1])
+            box[2].SetAnchor(0, 0.5)
+            box[2].x = 0
+            box[3].SetParent(box[1])
+            box[3].SetAnchor(1, 0.5)
+            box[3].x = 0
+        elseif timer > 40 and timer <= 80 then
+            dogSprite.xscale = timer < 80 and math.cos((timer - 40) / 5) or 0
+            box[1].xscale = (cursor.absx - (dogSprite.absx - (dogSprite.width / 2))) /  4
+            box[1].xscale = box[1].xscale * dogSprite.xscale
+            box[4].xscale = box[1].xscale
+        elseif timer > 80 and timer < 120 then
+            for _, box in pairs(box) do
+                box.alpha = box.alpha - (1/20)
+            end
+        -- remove sprites and event
+        elseif timer == 120 then
+            cursor.Remove()
+            cursor = nil
+            
+            for _, box in pairs(box) do
+                box.Remove()
+            end
+            box = nil
+        end
+        
+        General.Wait(1)
+    end
+    
+    -- end of event
+    General.Wait(160)
+    General.SetDialog({"[noskip][voice:v_asriel][waitall:5]...[waitall:1][w:40][noskip:off][mugshot:{Asriel/happyT,Asriel/happy,0.2}]Oh!",
+                       "[voice:v_asriel]Thank you, mister doggy!"}, true,
+                       {"Asriel/what",
+                       {"Asriel/happyT", "Asriel/happy", 0.2}})
+    Event.SetPage(Event.GetName(), -1)
 end
 
 -- Auto page used with StareTest
@@ -214,6 +302,11 @@ function EventPage4()
         SetGlobal("CYFOWStareSetDialog" .. i, nil)
     end
     Event.SetPage(Event.GetName(), 1)
+end
+
+-- General math function used with EventPage2 and EventPage69
+function lerp(a, b, t)
+    return a + ((b - a) * t)
 end
 
 --[[function EventPage1()
