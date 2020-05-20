87cc755dfe5a8c6cc97829da9e492ebed0d9bccb
CMD-God <48065803+CMD-God@users.noreply.github.com>
2020-05-20 10:03:23 +0300

    Added a bunch of comments.
    If something is not explained enough, pls message me.

--- Assets/Scripts/Device/DiscordControls.cs
+++ Assets/Scripts/Device/DiscordControls.cs
@@ -14,25 +14,30 @@ public class DiscordControls {
     // Use this for initialization
     public static void Start () {
 
-        discord = new Discord.Discord(711497963771527219, (System.UInt64)Discord.CreateFlags.NoRequireDiscord);
+        discord = new Discord.Discord(711497963771527219, (System.UInt64)Discord.CreateFlags.NoRequireDiscord); // Creates the object that manages the Rich Presence Commands. The first argument is the APPID, the second tells the libraries if Discord is a must or not.
         //ResetModName();
         //Clear();
     }
-
+    
+    // Returns the name with "Playing Mod: " attached to it.
     public static string getPlayingName(string name) {
         return "Playing Mod: " + name;
     }
     
+    // Sets the status to "Selecting a Mod", erasing details and timer
     public static void StartModSelect() {
         ClearRPVars(true, true);
         SetPresence("Selecting a Mod", "", 0);
     }
 
+    // Sets the status to "Playing a mod: ", erasing details and starting the timer
     public static void StartMod(string name) {
         ClearRPVars(true, true);
         SetPresence(getPlayingName(name), "", 1);
     }
 
+    // The function that sets the Discord Rich presence status. name's and details's default value make them not change, while time's default value removes the timer.
+    // The remaining boolean argument tells if Discord should run the timer backwards (as a stopwatch) or not.
     public static void SetPresence(string name = "", string details = "", int time = 0, bool remaining = false) {
 
         if (name != "") rpName = name;
@@ -49,13 +54,13 @@ public class DiscordControls {
         var activityManager = discord.GetActivityManager();
 
         var activity = new Discord.Activity {
-            State = rpDetails,
-            Details = rpName,
-            Timestamps = {
+            State = rpDetails, // The details (aka second row)
+            Details = rpName, // The top row
+            Timestamps = { // The timer being set up
                 Start = (remaining ? 0 : rpTime),
                 End = (remaining ? rpTime : 0)
             },
-            Assets = {
+            Assets = { // The CYF Logo
                 LargeImage = "cyf_logo",
                 LargeText = "Create Your Frisk"
             }
@@ -64,11 +69,14 @@ public class DiscordControls {
         activityManager.UpdateActivity(activity, (res) => {});
     }
     
+    // Since the SetPresence function's first two arguments' default values indicate that they should not be changed, this function resets the vales so they don't appear anymore on the next SetPresence call.
+    // (Yes, it's only use is to come right before SetPresence and SetPresence having the default value "")
     public static void ClearRPVars(bool name = false, bool details = false) {
         rpName = name ? "" : rpName;
         rpDetails = details ? "" : rpDetails;
     }
 
+    // Gets rid of the Discord Rich Presence thing ENTIRELY.
     public static void ClearPresence() {
         var activityManager = discord.GetActivityManager();
         activityManager.ClearActivity((result) => {});
