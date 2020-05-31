6705ef98d63ee066ddae5a0580f0f6acb8e6a6ae
WD200019 <warrend2000@gmail.com>
2020-05-31 10:32:06 -0500

    "Bundle all AssetBundles" & "Bundle one AssetBundle" options
    * Made separate options for building one shader AssetBundle vs all of them at a time
    * throw an error if in play mode or no name entered
    * moved the options to their own menu category, "CYF Shaders"

--- Assets/Editor/BundleShaders.cs
+++ Assets/Editor/BundleShaders.cs
@@ -6,15 +6,64 @@ using UnityEngine.UI;
 using System.IO;
 using System.Collections.Generic;
 
-public static class CreateAssetBundles {
+public static class BundleShaders {
     private static string assetBundleDirectory = "Assets/Editor/Output";
     private static string shaderDirectory = "Assets/Editor/Shaders";
 
-    [MenuItem("Assets/Bundle CYF Shaders...")]
-    static void BuildAllAssetBundles() {
-        if (!Directory.Exists(assetBundleDirectory))
-            Directory.CreateDirectory(assetBundleDirectory);
+    [MenuItem("CYF Shaders/Bundle all AssetBundles...")]
+    static void AllBundlesOption() {
+        if (EditorApplication.isPlaying) {
+            Debug.LogError("You may only build AssetBundles while not in play mode.");
+            return;
+        }
 
+        // Gets all AssetBundles from files within the shader directory
+        Dictionary<string, List<Shader>> bundles = RetrieveAllBundles();
+
+        // Build AssetBundles
+        BuildBundles(bundles);
+
+        EditorUtility.DisplayDialog("Bundling Shaders", "All CYF Shader Bundles have been created!\n\nYou can find them in:\n" + assetBundleDirectory, "OK");
+    }
+
+    [MenuItem("CYF Shaders/Bundle one AssetBundle...")]
+    static void OneBundleOption() {
+        if (EditorApplication.isPlaying) {
+            Debug.LogError("You may only build AssetBundles while not in play mode.");
+            return;
+        }
+
+        BundleShaderDialog window = (BundleShaderDialog)EditorWindow.GetWindow(typeof(BundleShaderDialog));
+        window.Show();
+    }
+
+    public static void OneBundle(string bundleName) {
+        // Gets all AssetBundles from files within the shader directory
+        Dictionary<string, List<Shader>> bundles = RetrieveAllBundles();
+
+        // Check if the given bundle name exists within this dictionary
+        if (bundleName == "") {
+            Debug.LogError("Please enter the name of an AssetBundle assigned in the Unity Editor.");
+            return;
+        }
+        if (!bundles.ContainsKey(bundleName)) {
+            Debug.LogError("The AssetBundle \"" + bundleName + "\" does not exist on any files in \"" + shaderDirectory + "\".");
+            return;
+        }
+
+        // Build all files with this bundle name into one AssetBundle
+        Dictionary<string, List<Shader>> bundlesToBuild = new Dictionary<string, List<Shader>>();
+        List<Shader> shaders = new List<Shader>();
+        foreach (Shader shader in bundles[bundleName])
+            shaders.Add(shader);
+        bundlesToBuild[bundleName] = shaders;
+
+        BuildBundles(bundlesToBuild);
+
+        EditorUtility.DisplayDialog("Bundling Shaders", "The CYF Shader Bundle \"" + bundleName + "\" has been created!\n\nYou can find it in:\n" + assetBundleDirectory + "/" + bundleName, "OK");
+    }
+
+    private static Dictionary<string, List<Shader>> RetrieveAllBundles() {
         // Get all assets
         string[] assets = Directory.GetFiles(shaderDirectory, "*.shader");
         Dictionary<string, List<Shader>> bundles = new Dictionary<string, List<Shader>>();
@@ -44,6 +93,13 @@ public static class CreateAssetBundles {
             }
         }
 
+        return bundles;
+    }
+
+    static void BuildBundles(Dictionary<string, List<Shader>> bundles) {
+        if (!Directory.Exists(assetBundleDirectory))
+            Directory.CreateDirectory(assetBundleDirectory);
+
         // Build AssetBundles
         foreach (KeyValuePair<string, List<Shader>> pair in bundles) {
             // Windows
@@ -59,7 +115,20 @@ public static class CreateAssetBundles {
                 Debug.LogError("An error occured while building the AssetBundle \"" + pair.Key + "\".");
             
         }
-
-        EditorUtility.DisplayDialog("Bundling Shaders", "All CYF Shaders have been bundled!\n\nYou can find them in:\n" + assetBundleDirectory, "OK");
     }
+}
+
+public class BundleShaderDialog : EditorWindow {
+    public string bundleName;
+
+    void OnGUI() {
+        bundleName = EditorGUILayout.TextField("AssetBundle Name", bundleName);
+
+        if (GUILayout.Button("Save")) {
+            OnClick();
+            GUIUtility.ExitGUI();
+        }
+    }
+
+    void OnClick() { BundleShaders.OneBundle(bundleName); }
 }
