604286ac0b7a0ce6e493d4e808301e9c7221a3d8
deo <dasdagon@gmail.com>
2021-06-11 21:32:45 +0400

    Remove defending state check

--- Assets/Scripts/Lua/CLRBindings/LuaArenaStatus.cs
+++ Assets/Scripts/Lua/CLRBindings/LuaArenaStatus.cs
@@ -29,71 +29,50 @@ public class LuaArenaStatus {
     /// <param name="w">New width for arena.</param>
     /// <param name="h">New height for arena.</param>
     public void Resize(int w, int h) {
-        AssertIsDefending();
         ArenaManager.instance.Resize(w, h);
     }
 
     public void ResizeImmediate(int w, int h) {
-        AssertIsDefending();
         ArenaManager.instance.ResizeImmediate(w, h);
     }
 
     public void Hide() {
-        AssertIsDefending();
         ArenaManager.instance.Hide();
     }
 
     public void Show() {
-        AssertIsDefending();
         ArenaManager.instance.Show();
     }
 
     public void Move(float x, float y, bool movePlayer = true, bool immediate = false) {
-        AssertIsDefending();
-
         if (immediate) ArenaManager.instance.MoveImmediate(x, y, movePlayer);
         else           ArenaManager.instance.Move(x, y, movePlayer);
     }
 
     public void MoveTo(float x, float y, bool movePlayer = true, bool immediate = false) {
-        AssertIsDefending();
-
         if (immediate) ArenaManager.instance.MoveToImmediate(x, y, movePlayer);
         else           ArenaManager.instance.MoveTo(x, y, movePlayer);
     }
 
     public void MoveAndResize(float x, float y, int width, int height, bool movePlayer = true, bool immediate = false) {
-        AssertIsDefending();
-
         if (immediate) ArenaManager.instance.MoveAndResizeImmediate(x, y, width, height, movePlayer);
         else           ArenaManager.instance.MoveAndResize(x, y, width, height, movePlayer);
     }
 
     public void MoveToAndResize(float x, float y, int width, int height, bool movePlayer = true, bool immediate = false) {
-        AssertIsDefending();
-
         if (immediate) ArenaManager.instance.MoveToAndResizeImmediate(x, y, width, height, movePlayer);
         else           ArenaManager.instance.MoveToAndResize(x, y, width, height, movePlayer);
     }
 
     public void SetSprite(string filename) {
-        AssertIsDefending();
         ArenaManager.instance.sprite.Set(filename);
     }
 
     public void SetColor(float[] color) {
-        AssertIsDefending();
         ArenaManager.instance.sprite.color = color;
     }
 
     public void SetColor32(float[] color32) {
-        AssertIsDefending();
         ArenaManager.instance.sprite.color32 = color32;
-    }
-
-    private void AssertIsDefending() {
-        if (UIController.instance.GetState() != UIController.UIState.DEFENDING) {
-            UnitaleUtil.DisplayLuaError("NOT THE WAVE SCRIPT", "sorry but pls don't");
-        }
     }
 }
