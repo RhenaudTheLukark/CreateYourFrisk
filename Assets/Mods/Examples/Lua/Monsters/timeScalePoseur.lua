d676873888f0e189d23b1ab1974908091c58a1d8
deo <dasdagon@gmail.com>
2021-06-17 15:49:44 +0400

    Add an example

---
+++ Assets/Mods/Examples/Lua/Monsters/timeScalePoseur.lua
@@ -0,0 +1,31 @@
+-- A basic monster script skeleton you can copy and modify for your own creations.
+comments = {"Smells like the work\rof an enemy stand.", "Poseur is posing like his\rlife depends on it.", "Poseur's limbs shouldn't be\rmoving in this way."}
+commands = {"Za Warudo"}
+randomdialogue = {"Random\nDialogue\n1.", "Random\nDialogue\n2.", "Random\nDialogue\n3."}
+
+sprite = "poseur" --Always PNG. Extension is added automatically.
+name = "Poseur"
+hp = 100
+atk = 1
+def = 1
+check = "Check message goes here."
+dialogbubble = "right" -- See documentation for what bubbles you have available.
+canspare = false
+
+-- Happens after the slash animation but before 
+function HandleAttack(attackstatus)
+    if attackstatus == -1 then
+        -- player pressed fight but didn't press Z afterwards
+    else
+        -- player did actually attack
+    end
+end
+ 
+-- This handles the commands; all-caps versions of the commands list you have above.
+function HandleCustomCommand(command)
+    if command == "ZA WARUDO" then
+        Time.timeScale = Time.timeScale / 2
+        currentdialogue = {"Time\nwill\ngo\nslower!"}
+        BattleDialog({"You selected Za Warudo."})
+    end
+end
