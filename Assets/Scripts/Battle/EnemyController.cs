RhenaudTheLukark/CreateYourFrisk

commit e6d24d55b41cad130e604306d5259c7f00fde7de

Add HandleFlee (#93)
* Add HandleFlee() and Flee()

Assets/Scripts/Battle/EnemyEncounter.cs
@@ -57,6 +57,7 @@ public class EnemyEncounter : MonoBehaviour {
             script.Bind("CreateProjectileAbs", (Func<Script, string, float, float, string, DynValue>)CreateProjectileAbs);
             script.Bind("SetButtonLayer", (Action<string>)LuaScriptBinder.SetButtonLayer);
             script.Bind("CreateEnemy", (Func<string, float, float, DynValue>)CreateEnemy);
+            script.Bind("Flee", (Action)Flee);
             return true;
         }
     }
@@ -75,6 +76,10 @@ public class EnemyEncounter : MonoBehaviour {
         enemies.Add(enemyController);
         enemyController.InitializeEnemy();
         return UserData.Create(enemyController.script);
+    }
+
+    public void Flee() {
+        StartCoroutine(UIController.instance.ISuperFlee());
     }
 
     public bool CallOnSelfOrChildren(string func, DynValue[] param = null) {

Assets/Scripts/Battle/UIController.cs
@@ -976,8 +976,20 @@ public class UIController : MonoBehaviour {
                         }
                         case 1: {
                             if (!GlobalControls.retroMode) {
-                                if ((EnemyEncounter.script.GetVar("fleesuccess").Type != DataType.Boolean && (Math.RandomRange(0, 9) + encounter.turnCount) > 4)
-                                 || EnemyEncounter.script.GetVar("fleesuccess").Boolean)
+                                bool fleeSuccess = (
+                                    EnemyEncounter.script.GetVar("fleesuccess").Type != DataType.Boolean
+                                    && (Math.RandomRange(0, 9) + encounter.turnCount) > 4
+                                ) || EnemyEncounter.script.GetVar("fleesuccess").Boolean;
+
+                                bool called = encounter.CallOnSelfOrChildren(
+                                    "HandleFlee", new DynValue[] {DynValue.NewBoolean(fleeSuccess)}
+                                );
+
+                                if (called) {
+                                    break;
+                                }
+
+                                if (fleeSuccess)
                                     StartCoroutine(ISuperFlee());
                                 else
                                     SwitchState(UIState.ENEMYDIALOGUE);
@@ -1423,7 +1435,7 @@ public class UIController : MonoBehaviour {
             ActionDialogResult(new RegularMessage("YOU WON!\nYou earned " + exp + " XP and " + gold + " gold."), UIState.DONE);
     }
 
-    private IEnumerator ISuperFlee() {
+    public IEnumerator ISuperFlee() {
         PlayerController.instance.GetComponent<Image>().enabled = false;
         UnitaleUtil.PlaySound("Mercy", "runaway");
 

Documentation CYF 1.0/documentation.html
@@ -38,7 +38,7 @@ it's w3c valid, at least
                     <li><a href="pages/terms.html">Terminology</a></li>
                 <li class="li-header">API</li>
                     <li class="new066"><a href="pages/api-text.html">Text commands</a></li>
-                    <li class="new"><a href="pages/api-events.html">Game events</a></li>
+                    <li class="new066"><a href="pages/api-events.html">Game events</a></li>
                     <hr style="margin-top:-2.5px;margin-bottom:2.5px;">
                     <li class="li-indent">Functions &amp; Objects:</li>
                         <li class="li-indent new"><a href="pages/api-functions-main.html">Misc. Functions</a></li>

Documentation CYF 1.0/pages/api-animation.html
@@ -43,7 +43,7 @@ it's w3c valid, at least
                     <li><a href="terms.html">Terminology</a></li>
                 <li class="li-header">API</li>
                     <li class="new066"><a href="api-text.html">Text commands</a></li>
-                    <li class="new"><a href="api-events.html">Game events</a></li>
+                    <li class="new066"><a href="api-events.html">Game events</a></li>
                     <hr style="margin-top:-2.5px;margin-bottom:2.5px;">
                     <li class="li-indent">Functions &amp; Objects:</li>
                         <li class="new066 li-indent"><a href="api-functions-main.html">Misc. Functions</a></li>

commit 884a00a5d36a1488cdfa6f1ea1f8c0056c83e7db

Removed silly fleeing messages & Unified TryCall across script types & Fixed standard flee routine & Now EXP and Gold gains are applied upon fleeing
- Create Your Frisk's old fleeing messages have been replaced by
  Undertale's standard fleeing messages (although the old messages are
  still commented where they were before).

- The TryCall function is a function that is used to call a function
  from a given Lua script. It was defined in each script handling a type
  of script (enemy, encounter, wave, shop...), and some implementations
  were doing things differently than others despite being the same
  function. TryCall has now been moved to UnitaleUtil, and each class
  using it now uses the same function across the entire engine.

- The fleeing routine was broken in (#93) because of the differences of
  implementation of TryCall fixed above. Now the standard fleeing
  routine works properly.

- When fleeing a battle and an enemy has been spared or killed, the
  experience and gold gains weren't applied. Now they are.

Assets/Scripts/Battle/EnemyController.cs
@@ -294,7 +294,7 @@ public class EnemyController : MonoBehaviour {
         catch (Exception ex)            { UnitaleUtil.DisplayLuaError(scriptName, "Unknown error. Usually means you're missing a sprite.\nSee documentation for details.\nStacktrace below in case you wanna notify a dev.\n\nError: " + ex.Message + "\n\n" + ex.StackTrace); }
     }
 
-    public void HandleAttack(int hitStatus) { TryCall("HandleAttack", new[] { DynValue.NewNumber(hitStatus) }); }
+    public void HandleAttack(int hitStatus) { UnitaleUtil.TryCall(script, "HandleAttack", new[] { DynValue.NewNumber(hitStatus) }); }
 
     public string[] GetDefenseDialog() {
         DynValue dialogues = script.GetVar("currentdialogue");
@@ -312,20 +312,8 @@ public class EnemyController : MonoBehaviour {
         return dialogueStrings;
     }
 
-    public bool TryCall(string func, DynValue[] param = null) {
-        try {
-            DynValue sval = script.GetVar(func);
-            if (sval == null || sval.Type == DataType.Nil) return false;
-            if (param != null)                             script.Call(func, param);
-            else                                           script.Call(func);
-            return true;
-        }
-        catch (InterpreterException ex) { UnitaleUtil.DisplayLuaError(scriptName, UnitaleUtil.FormatErrorSource(ex.DecoratedMessage, ex.Message) + ex.Message); }
-        return true;
-    }
-
     protected void HandleCustomCommand(string command) {
-        TryCall("HandleCustomCommand", new[] { DynValue.NewString(command) });
+        UnitaleUtil.TryCall(script, "HandleCustomCommand", new[] { DynValue.NewString(command) });
     }
 
     public void Remove() {

Assets/Scripts/Battle/EnemyEncounter.cs
@@ -83,30 +83,13 @@ public class EnemyEncounter : MonoBehaviour {
     }
 
     public bool CallOnSelfOrChildren(string func, DynValue[] param = null) {
-        bool result = param != null ? TryCall(func, param) : TryCall(func);
+        if (UnitaleUtil.TryCall(script, func, param)) return true;
 
-        if (result) return true;
         bool calledOne = false;
-        foreach (EnemyController enemy in enemies) {
-            if (param != null) {
-                if (enemy.TryCall(func, param))
-                    calledOne = true;
-            } else if (enemy.TryCall(func))
+        foreach (EnemyController enemy in enemies)
+            if (UnitaleUtil.TryCall(enemy.script, func, param))
                 calledOne = true;
-        }
         return calledOne;
-    }
-
-    public bool TryCall(string func, DynValue[] param = null) {
-        try {
-            if (script.GetVar(func) == null) return false;
-            if (param != null)               script.Call(func, param);
-            else                             script.Call(func);
-            return true;
-        } catch (InterpreterException ex) {
-            UnitaleUtil.DisplayLuaError(StaticInits.ENCOUNTER, UnitaleUtil.FormatErrorSource(ex.DecoratedMessage, ex.Message) + ex.Message);
-            return true;
-        }
     }
 
     public EnemyController[] EnabledEnemies {

Assets/Scripts/Battle/FightUI.cs
@@ -98,7 +98,7 @@ public class FightUI : MonoBehaviour {
         PlayerController.instance.lastHitMult = FightUIController.instance.getAtkMult();
         bool damagePredefined = Damage != FightUIController.DAMAGE_NOT_SET;
         stopped = true;
-        enemy.TryCall("BeforeDamageCalculation");
+        UnitaleUtil.TryCall(enemy.script, "BeforeDamageCalculation");
         if (!damagePredefined)
             Damage = FightUIController.instance.getDamage(enemy, atkMult);
         UpdateSlicePos();
@@ -138,7 +138,7 @@ public class FightUI : MonoBehaviour {
                 wait1frame = true;
                 slice.StopAnimation();
                 slice.Set("empty");
-                enemy.TryCall("BeforeDamageValues", new[] { DynValue.NewNumber(Damage) });
+                UnitaleUtil.TryCall(enemy.script, "BeforeDamageValues", new[] { DynValue.NewNumber(Damage) });
                 if (Damage > 0) {
                     AudioSource aSrc = GetComponent<AudioSource>();
                     aSrc.clip = AudioClipRegistry.GetSound("hitsound");

Assets/Scripts/Battle/UIController.cs
@@ -112,6 +112,7 @@ public class UIController : MonoBehaviour {
     }
 
     public static void EndBattle(bool fromGameOver = false) {
+        MusicManager.SetSoundDictionary("RESETDICTIONARY", "");
         LuaSpriteController spr = (LuaSpriteController)SpriteUtil.MakeIngameSprite("black", -1).UserData.Object;
         if (GameObject.Find("TopLayer"))
             spr.layer = "Top";
@@ -964,7 +965,7 @@ public class UIController : MonoBehaviour {
                                 if (!enabledEnTemp.Contains(encounter.enemies[i]))
                                     continue;
                                 if (!canSpare[i]) continue;
-                                if (!encounter.enemies[i].TryCall("OnSpare"))
+                                if (!UnitaleUtil.TryCall(encounter.enemies[i].script, "OnSpare"))
                                     encounter.enemies[i].DoSpare();
                                 else
                                     spareList[i] = true;
@@ -985,23 +986,13 @@ public class UIController : MonoBehaviour {
                         }
                         case 1: {
                             if (!GlobalControls.retroMode) {
-                                bool fleeSuccess = (
-                                    EnemyEncounter.script.GetVar("fleesuccess").Type != DataType.Boolean
-                                    && (Math.RandomRange(0, 9) + encounter.turnCount) > 4
-                                ) || EnemyEncounter.script.GetVar("fleesuccess").Boolean;
+                                bool fleeSuccess = EnemyEncounter.script.GetVar("fleesuccess").Boolean || EnemyEncounter.script.GetVar("fleesuccess").Type != DataType.Boolean && Math.RandomRange(0, 9) + encounter.turnCount > 4;
 
-                                bool called = encounter.CallOnSelfOrChildren(
-                                    "HandleFlee", new DynValue[] {DynValue.NewBoolean(fleeSuccess)}
-                                );
-
-                                if (called) {
+                                if (encounter.CallOnSelfOrChildren("HandleFlee", new[] { DynValue.NewBoolean(fleeSuccess) }))
                                     break;
-                                }
 
-                                if (fleeSuccess)
-                                    StartCoroutine(ISuperFlee());
-                                else
-                                    SwitchState("ENEMYDIALOGUE");
+                                if (fleeSuccess) StartCoroutine(ISuperFlee());
+                                else             SwitchState("ENEMYDIALOGUE");
                             } else {
                                 PlayerController.instance.GetComponent<Image>().enabled = false;
                                 AudioClip yay = AudioClipRegistry.GetSound("runaway");
@@ -1434,7 +1425,6 @@ public class UIController : MonoBehaviour {
         Camera.main.GetComponent<AudioSource>().Stop();
         bool levelUp = PlayerCharacter.instance.AddBattleResults(exp, gold);
         Inventory.RemoveAddedItems();
-        MusicManager.SetSoundDictionary("RESETDICTIONARY", "");
         if (levelUp && exp != 0) {
             UIStats.instance.setPlayerInfo(PlayerCharacter.instance.Name, PlayerCharacter.instance.LV);
             UIStats.instance.setMaxHP();
@@ -1454,18 +1444,32 @@ public class UIController : MonoBehaviour {
             for (int i = 0; i < tempFleeTexts.Table.Length; i++)
                 fleeTexts.Add(tempFleeTexts.Table.Get(i + 1).String);
         else {
-            fleeTexts = new List<string> { "I'm outta here.",  "I've got better things to do.", "Don't waste my time.",
+            /*fleeTexts = new List<string> { "I'm outta here.",  "I've got better things to do.", "Don't waste my time.",
                                            "Nah, I don't like you.", "I just wanted to walk\ra bit. Leave me alone.", "You're cute, I won't kill you :3",
                                            "Better safe than sorry.", "Do as if you never saw\rthem and walk away.", "I'll kill you last.",
                                            "Nope. [w:5]Nope. Nope. Nope. Nope.", "Wait for me, Rhenaud!", "Flee like sissy!" };
             if (!ControlPanel.instance.Safe) {
                 fleeTexts.Add("I've got shit to do.");
                 fleeTexts.Add("Fuck this shit I'm out.");
-            }
+            }*/
+            if (exp > 0 || gold > 0) {
+                string fleeString = "Ran away with " + exp + " EXP\rand " + gold + " GOLD.";
+                bool levelUp = PlayerCharacter.instance.AddBattleResults(exp, gold);
+                if (levelUp && exp > 0) {
+                    UIStats.instance.setPlayerInfo(PlayerCharacter.instance.Name, PlayerCharacter.instance.LV);
+                    UIStats.instance.setMaxHP();
+                    UIStats.instance.setHP(PlayerCharacter.instance.HP);
+                    fleeString = "[sound:levelup]" + fleeString + "\nYour LOVE increased.";
+                }
+                fleeTexts = new List<string> { fleeString };
+            } else
+                fleeTexts = new List<string> {
+                    "Escaped...",
+                    "Don't slow me down.",
+                    "I've got better to do.",
+                    "I'm outta here."
+                };
         }
-
-        /*string[] text = { "See mom, I can flee!", "LEGZ!", "It looks more like a\nreal flee.", "/me flees", "*flees*", "To infinity and beyond!",
-                            "Yeah, that's the secret.\nI hope you liked it!"};*/
 
         ActionDialogResult(new TextMessage[] { new RegularMessage(fleeTexts[Math.RandomRange(0, fleeTexts.Count)]) });
         fleeSwitch = true;
@@ -1489,7 +1493,7 @@ public class UIController : MonoBehaviour {
         if (encounter.gameOverStance)
             return;
         if (encounterHasUpdate)
-            encounter.TryCall("Update");
+            UnitaleUtil.TryCall(EnemyEncounter.script, "Update");
 
         if (frozenState != "PAUSE")
             return;
@@ -1543,7 +1547,7 @@ public class UIController : MonoBehaviour {
                 int hp = enemyController.HP;
                 if (hp > 0 || enemyController.Unkillable) continue;
                 // fightUI.disableImmediate();
-                if (enemyController.TryCall("OnDeath")) continue;
+                if (UnitaleUtil.TryCall(enemyController.script, "OnDeath")) continue;
                 noOnDeath = false;
                 enemyController.DoKill();
 
@@ -1568,7 +1572,7 @@ public class UIController : MonoBehaviour {
         for (int i = 0; i < spareList.Length; i++) {
             if (!spareList[i] || encounter.enemies[i].spared) continue;
             state = "SPAREIDLE";
-            encounter.enemies[i].TryCall("OnSpare");
+            UnitaleUtil.TryCall(encounter.enemies[i].script, "OnSpare");
             toSpare = true;
         }
         if (!toSpare)

Assets/Scripts/Inventory/Inventory.cs
@@ -67,23 +67,8 @@ public static class Inventory {
         return type;
     }
 
-    private static void CallOnSelf(string func, DynValue[] param = null) {
-        if (param != null) TryCall(func, param);
-        else               TryCall(func);
-    }
-
     public static bool TryCall(string func, DynValue[] param = null) {
-        if (UnitaleUtil.IsOverworld) return false;
-        try {
-            if (EnemyEncounter.script.GetVar(func) == null)
-                return false;
-            if (param != null)  EnemyEncounter.script.Call(func, param);
-            else                EnemyEncounter.script.Call(func);
-            return true;
-        } catch (InterpreterException ex) {
-            UnitaleUtil.DisplayLuaError(StaticInits.ENCOUNTER, UnitaleUtil.FormatErrorSource(ex.DecoratedMessage, ex.Message) + ex.Message);
-            return true;
-        }
+        return !UnitaleUtil.IsOverworld && UnitaleUtil.TryCall(EnemyEncounter.script, func, param);
     }
 
     public static void UseItem(int ID) {
@@ -93,7 +78,7 @@ public static class Inventory {
         //bool inverseRemove = false;
         int type = inventory[ID].Type;
         float amount;
-        CallOnSelf("HandleItem", new[] { DynValue.NewString(Name.ToUpper()), DynValue.NewNumber(ID + 1) });
+        TryCall("HandleItem", new[] { DynValue.NewString(Name.ToUpper()), DynValue.NewNumber(ID + 1) });
 
         TextMessage[] mess = { };
         if (addedItems.Count != 0) {

Assets/Scripts/Overworld/ShopScript.cs
@@ -53,7 +53,7 @@ public class ShopScript : MonoBehaviour {
             script.script.Globals["CreateSprite"] = (Func<string, string, int, DynValue>) SpriteUtil.MakeIngameSprite;
             script.script.Globals["CreateLayer"] = (Func<string, string, bool, bool>) SpriteUtil.CreateLayer;
             script.script.Globals["CreateText"] = (Func<Script, DynValue, DynValue, int, string, int, LuaTextManager>) LuaScriptBinder.CreateText;
-            TryCall("Start");
+            UnitaleUtil.TryCall(script, "Start");
 
             tmMain.SetCaller(script);
             tmChoice.SetCaller(script);
@@ -92,7 +92,7 @@ public class ShopScript : MonoBehaviour {
 
     private void Interrupt(DynValue text, string nextState = "MENU") {
         if (currentState == State.INTERRUPT) return;
-        TryCall("OnInterrupt", DynValue.NewString(nextState));
+        UnitaleUtil.TryCall(script, "OnInterrupt", DynValue.NewString(nextState));
         try { interruptState = (State)Enum.Parse(typeof(State), nextState, true); }
         catch { throw new CYFException("\"" + nextState + "\" is not a valid shop state."); }
         ChangeState(State.INTERRUPT, -1, text);
@@ -115,7 +115,7 @@ public class ShopScript : MonoBehaviour {
         string text;
         switch (state) {
             case State.MENU:
-                TryCall("EnterMenu");
+                UnitaleUtil.TryCall(script, "EnterMenu");
                 if (!interrupted) {
                     sellItem = -1;
                     if (select != -1)
@@ -144,7 +144,7 @@ public class ShopScript : MonoBehaviour {
                                                                "\n\n\t   [color:ffff00](" + PlayerCharacter.instance.Gold + "G)", false, true) });
                 break;
             case State.BUY:
-                TryCall("EnterBuy");
+                UnitaleUtil.TryCall(script, "EnterBuy");
                 if (!interrupted) {
                     if (select != -1)
                         selection = select;
@@ -157,7 +157,7 @@ public class ShopScript : MonoBehaviour {
                 }
                 break;
             case State.SELL:
-                TryCall("EnterSell");
+                UnitaleUtil.TryCall(script, "EnterSell");
                 if (!interrupted) {
                     if (select != -1)
                         selection = select;
@@ -167,14 +167,14 @@ public class ShopScript : MonoBehaviour {
                     numberOfChoices = Inventory.inventory.Count + 1;
                     tmBigTalk.SetTextQueue(new[] { new TextMessage("[noskipatall][novoice][font:uidialoglilspace][linespacing:11]" + text, false, true) });
                     if (Inventory.inventory.Count == 0) {
-                        TryCall("FailSell", DynValue.NewString("empty"));
+                        UnitaleUtil.TryCall(script, "FailSell", DynValue.NewString("empty"));
                         if (!interrupted)
                             HandleCancel();
                     }
                 }
                 break;
             case State.TALK:
-                TryCall("EnterTalk");
+                UnitaleUtil.TryCall(script, "EnterTalk");
                 if (!interrupted) {
                     if (select != -1)
                         selection = select;
@@ -185,7 +185,7 @@ public class ShopScript : MonoBehaviour {
                 }
                 break;
             case State.TALKINPROGRESS:
-                TryCall("SuccessTalk", DynValue.NewString(mainName[selection]));
+                UnitaleUtil.TryCall(script, "SuccessTalk", DynValue.NewString(mainName[selection]));
                 if (!interrupted) {
                     TextMessage[] texts = BuildTalkResultStrings();
                     tmBigTalk.SetTextQueue(texts);
@@ -193,7 +193,7 @@ public class ShopScript : MonoBehaviour {
                 }
                 break;
             case State.EXIT:
-                TryCall("EnterExit");
+                UnitaleUtil.TryCall(script, "EnterExit");
                 if (!interrupted) {
                     TextMessage[] texts2 = BuildTextFromTable(script.GetVar("exittalk"), "[linespacing:11]");
                     tmBigTalk.SetTextQueue(texts2);
@@ -362,7 +362,7 @@ public class ShopScript : MonoBehaviour {
                 else {
                     ChangeState(State.BUYCONFIRM, 0);
                     if (mainPrice[currentItemIndex] == 0) {
-                        TryCall("FailBuy", DynValue.NewString("soldout"));
+                        UnitaleUtil.TryCall(script, "FailBuy", DynValue.NewString("soldout"));
                         HandleCancel();
                     }
                 }
@@ -373,7 +373,7 @@ public class ShopScript : MonoBehaviour {
                     ChangeState(State.SELLCONFIRM, 0);
                     if (Inventory.NametoPrice[Inventory.inventory[currentItemIndex].Name] == 0) {
                         UnitaleUtil.PlaySound("SeparateSound", "ShopFail");
-                        TryCall("FailSell", DynValue.NewString("cantsell"));
+                        UnitaleUtil.TryCall(script, "FailSell", DynValue.NewString("cantsell"));
                         HandleCancel();
                     }
                 }
@@ -383,13 +383,13 @@ public class ShopScript : MonoBehaviour {
                 else                                  ChangeState(State.TALKINPROGRESS);
                 break;
             case State.BUYCONFIRM:
-                if (selection == numberOfChoices - 1) TryCall("ReturnBuy");
+                if (selection == numberOfChoices - 1) UnitaleUtil.TryCall(script, "ReturnBuy");
                 else {
-                    if (PlayerCharacter.instance.Gold < mainPrice[currentItemIndex]) TryCall("FailBuy", DynValue.NewString("gold"));
-                    else if (Inventory.inventory.Count == Inventory.inventorySize)   TryCall("FailBuy", DynValue.NewString("full"));
-                    else if (mainPrice[currentItemIndex] == 0)                       TryCall("FailBuy", DynValue.NewString("soldout"));
+                    if (PlayerCharacter.instance.Gold < mainPrice[currentItemIndex]) UnitaleUtil.TryCall(script, "FailBuy", DynValue.NewString("gold"));
+                    else if (Inventory.inventory.Count == Inventory.inventorySize)   UnitaleUtil.TryCall(script, "FailBuy", DynValue.NewString("full"));
+                    else if (mainPrice[currentItemIndex] == 0)                       UnitaleUtil.TryCall(script, "FailBuy", DynValue.NewString("soldout"));
                     else {
-                        TryCall("SuccessBuy", DynValue.NewString(mainName[currentItemIndex]));
+                        UnitaleUtil.TryCall(script, "SuccessBuy", DynValue.NewString(mainName[currentItemIndex]));
                         UnitaleUtil.PlaySound("SeparateSound", "ShopSuccess");
                         PlayerCharacter.instance.SetGold(PlayerCharacter.instance.Gold - mainPrice[currentItemIndex]);
                         Inventory.AddItem(mainName[currentItemIndex]);
@@ -403,9 +403,9 @@ public class ShopScript : MonoBehaviour {
                 }
                 break;
             case State.SELLCONFIRM:
-                if (selection == numberOfChoices - 1) TryCall("ReturnSell");
+                if (selection == numberOfChoices - 1) UnitaleUtil.TryCall(script, "ReturnSell");
                 else {
-                    TryCall("SuccessSell", DynValue.NewString(mainName[currentItemIndex]));
+                    UnitaleUtil.TryCall(script, "SuccessSell", DynValue.NewString(mainName[currentItemIndex]));
                     UnitaleUtil.PlaySound("SeparateSound", "ShopSuccess");
                     PlayerCharacter.instance.SetGold(PlayerCharacter.instance.Gold + Inventory.NametoPrice[Inventory.inventory[currentItemIndex].Name] / 5);
                     Inventory.RemoveItem(currentItemIndex);
@@ -519,22 +519,10 @@ public class ShopScript : MonoBehaviour {
         }
     }
 
-    public bool TryCall(string func, DynValue param) { return TryCall(func, new[] { param }); }
-    public bool TryCall(string func, DynValue[] param = null) {
-        try {
-            DynValue sval = script.GetVar(func);
-            if (sval == null || sval.Type == DataType.Nil) return false;
-            if (param != null) script.Call(func, param);
-            else script.Call(func);
-            return true;
-        } catch (InterpreterException ex) { UnitaleUtil.DisplayLuaError(scriptName, UnitaleUtil.FormatErrorSource(ex.DecoratedMessage, ex.Message) + ex.Message); }
-        return true;
-    }
-
     // Update is called once per frame
     private void Update() {
         if (script.GetVar("Update") != null)
-            TryCall("Update");
+            UnitaleUtil.TryCall(script, "Update");
         if (waitForSelection) {
             SetPlayerOnSelection();
             waitForSelection = false;

Assets/Scripts/PlayerControllers/PlayerController.cs
@@ -173,7 +173,7 @@ public class PlayerController : MonoBehaviour {
         if (newhp <= 0) {
             deathEscape = false;
             if (GlobalControls.isInFight) {
-                UIController.instance.encounter.TryCall("BeforeDeath");
+                UnitaleUtil.TryCall(EnemyEncounter.script, "BeforeDeath");
                 if (deathEscape)
                     return;
 

Assets/Scripts/Util/UnitaleUtil.cs
@@ -629,4 +629,14 @@ public static class UnitaleUtil {
     public static bool IsSpecialAnnouncement(string str) {
         return str == "4eab1af3ab6a932c23b3cdb8ef618b1af9c02088";
     }
+
+    public static bool TryCall(ScriptWrapper script, string func, DynValue param) { return TryCall(script, func, new[] { param }); }
+    public static bool TryCall(ScriptWrapper script, string func, DynValue[] param = null) {
+        try {
+            DynValue sval = script.GetVar(func);
+            if (sval == null || sval.Type == DataType.Nil) return false;
+            script.Call(func, param);
+        } catch (InterpreterException ex) { DisplayLuaError(script.scriptname, FormatErrorSource(ex.DecoratedMessage, ex.Message) + ex.Message); }
+        return true;
+    }
 }