8719659a6f39c1bc709f7f8fdb93466cd1ff5abe
UndertaleFan54 <mynamajeff@horsefucker.org>
2023-07-15 05:34:16 -0400

    a

--- Assets/Editor/UnityBuilderAction/BuildScript.cs
+++ Assets/Editor/UnityBuilderAction/BuildScript.cs
@@ -79,81 +79,73 @@ namespace UnityBuilderAction
             Build(buildTarget, buildSubtarget, options["customBuildPath"]);
         }
 
-        // private static Dictionary<string, string> GetValidatedOptions()
-        // {
-        //     ParseCommandLineArguments(out Dictionary<string, string> validatedOptions);
+        private static Dictionary<string, string> GetValidatedOptions()
+        {
+            ParseCommandLineArguments(out Dictionary<string, string> validatedOptions);
 
-        //     // if (!validatedOptions.TryGetValue("projectPath", out string _))
-        //     // {
-        //     //     Console.WriteLine("Missing argument -projectPath");
-        //     //     EditorApplication.Exit(110);
-        //     // }
+            // if (!validatedOptions.TryGetValue("projectPath", out string _))
+            // {
+            //     Console.WriteLine("Missing argument -projectPath");
+            //     EditorApplication.Exit(110);
+            // }
 
-        //     // if (!validatedOptions.TryGetValue("buildTarget", out string buildTarget))
-        //     // {
-        //     //     Console.WriteLine("Missing argument -buildTarget");
-        //     //     EditorApplication.Exit(120);
-        //     // }
+            // if (!validatedOptions.TryGetValue("buildTarget", out string buildTarget))
+            // {
+            //     Console.WriteLine("Missing argument -buildTarget");
+            //     EditorApplication.Exit(120);
+            // }
 
-        //     // if (!Enum.IsDefined(typeof(BuildTarget), buildTarget ?? string.Empty))
-        //     // {
-        //     //     Console.WriteLine($"{buildTarget} is not a defined {nameof(BuildTarget)}");
-        //     //     EditorApplication.Exit(121);
-        //     // }
+            // if (!Enum.IsDefined(typeof(BuildTarget), buildTarget ?? string.Empty))
+            // {
+            //     Console.WriteLine($"{buildTarget} is not a defined {nameof(BuildTarget)}");
+            //     EditorApplication.Exit(121);
+            // }
 
-        //     // if (!validatedOptions.TryGetValue("customBuildPath", out string _))
-        //     // {
-        //     //     Console.WriteLine("Missing argument -customBuildPath");
-        //     //     EditorApplication.Exit(130);
-        //     // }
+            // if (!validatedOptions.TryGetValue("customBuildPath", out string _))
+            // {
+            //     Console.WriteLine("Missing argument -customBuildPath");
+            //     EditorApplication.Exit(130);
+            // }
 
-        //     // const string defaultCustomBuildName = "TestBuild";
-        //     // if (!validatedOptions.TryGetValue("customBuildName", out string customBuildName))
-        //     // {
-        //     //     Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
-        //     //     validatedOptions.Add("customBuildName", defaultCustomBuildName);
-        //     // }
-        //     // else if (customBuildName == "")
-        //     // {
-        //     //     Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
-        //     //     validatedOptions.Add("customBuildName", defaultCustomBuildName);
-        //     // }
+            // const string defaultCustomBuildName = "TestBuild";
+            // if (!validatedOptions.TryGetValue("customBuildName", out string customBuildName))
+            // {
+            //     Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+            //     validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            // }
+            // else if (customBuildName == "")
+            // {
+            //     Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+            //     validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            // }
 
-        //     return validatedOptions;
-        // }
+            return validatedOptions;
+        }
 
-        // private static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
-        // {
-        //     providedArguments = new Dictionary<string, string>();
-        //     string[] args = Environment.GetCommandLineArgs();
+        private static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
+        {
+            providedArguments = new Dictionary<string, string>();
+            string[] args = Environment.GetCommandLineArgs();
 
-        //     Console.WriteLine(
-        //         $"{Eol}" +
-        //         $"###########################{Eol}" +
-        //         $"#    Parsing settings     #{Eol}" +
-        //         $"###########################{Eol}" +
-        //         $"{Eol}"
-        //     );
+            // Extract flags with optional values
+            for (int current = 0, next = 1; current < args.Length; current++, next++)
+            {
+                // Parse flag
+                bool isFlag = args[current].StartsWith("-");
+                if (!isFlag) continue;
+                string flag = args[current].TrimStart('-');
 
-        //     // Extract flags with optional values
-        //     for (int current = 0, next = 1; current < args.Length; current++, next++)
-        //     {
-        //         // Parse flag
-        //         bool isFlag = args[current].StartsWith("-");
-        //         if (!isFlag) continue;
-        //         string flag = args[current].TrimStart('-');
+                // Parse optional value
+                bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                string value = flagHasValue ? args[next].TrimStart('-') : "";
+                bool secret = Secrets.Contains(flag);
+                string displayValue = secret ? "*HIDDEN*" : "\"" + value + "\"";
 
-        //         // Parse optional value
-        //         bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
-        //         string value = flagHasValue ? args[next].TrimStart('-') : "";
-        //         bool secret = Secrets.Contains(flag);
-        //         string displayValue = secret ? "*HIDDEN*" : "\"" + value + "\"";
-
-        //         // Assign
-        //         Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
-        //         providedArguments.Add(flag, value);
-        //     }
-        // }
+                // Assign
+                // Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
+                providedArguments.Add(flag, value);
+            }
+        }
 
         private static void Build(BuildTarget buildTarget, int buildSubtarget, string filePath)
         {
