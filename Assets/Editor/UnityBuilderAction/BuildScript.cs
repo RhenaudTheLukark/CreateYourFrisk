906672de72b035386015107b3deea4141ececd22
UndertaleFan54 <mynamajeff@horsefucker.org>
2023-07-15 06:32:46 -0400

    cleanup commented code in BuildScript

--- Assets/Editor/UnityBuilderAction/BuildScript.cs
+++ Assets/Editor/UnityBuilderAction/BuildScript.cs
@@ -28,49 +28,6 @@ namespace UnityBuilderAction
 
             // Apply build target
             var buildTarget = (BuildTarget) Enum.Parse(typeof(BuildTarget), options["buildTarget"]);
-            // switch (buildTarget)
-            // {
-            //     case BuildTarget.Android:
-            //     {
-            //         EditorUserBuildSettings.buildAppBundle = options["customBuildPath"].EndsWith(".aab");
-            //         if (options.TryGetValue("androidKeystoreName", out string keystoreName) &&
-            //             !string.IsNullOrEmpty(keystoreName))
-            //         {
-            //           PlayerSettings.Android.useCustomKeystore = true;
-            //           PlayerSettings.Android.keystoreName = keystoreName;
-            //         }
-            //         if (options.TryGetValue("androidKeystorePass", out string keystorePass) &&
-            //             !string.IsNullOrEmpty(keystorePass))
-            //             PlayerSettings.Android.keystorePass = keystorePass;
-            //         if (options.TryGetValue("androidKeyaliasName", out string keyaliasName) &&
-            //             !string.IsNullOrEmpty(keyaliasName))
-            //             PlayerSettings.Android.keyaliasName = keyaliasName;
-            //         if (options.TryGetValue("androidKeyaliasPass", out string keyaliasPass) &&
-            //             !string.IsNullOrEmpty(keyaliasPass))
-            //             PlayerSettings.Android.keyaliasPass = keyaliasPass;
-            //         if (options.TryGetValue("androidTargetSdkVersion", out string androidTargetSdkVersion) &&
-            //             !string.IsNullOrEmpty(androidTargetSdkVersion))
-            //         {
-            //             var targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
-            //             try
-            //             {
-            //                 targetSdkVersion =
-            //                     (AndroidSdkVersions) Enum.Parse(typeof(AndroidSdkVersions), androidTargetSdkVersion);
-            //             }
-            //             catch
-            //             {
-            //                 UnityEngine.Debug.Log("Failed to parse androidTargetSdkVersion! Fallback to AndroidApiLevelAuto");
-            //             }
-
-            //             PlayerSettings.Android.targetSdkVersion = targetSdkVersion;
-            //         }
-
-            //         break;
-            //     }
-            //     case BuildTarget.StandaloneOSX:
-            //         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.Mono2x);
-            //         break;
-            // }
 
             // Determine subtarget
             int buildSubtarget = 0;
@@ -83,41 +40,7 @@ namespace UnityBuilderAction
         {
             Dictionary<string, string> validatedOptions = ParseCommandLineArguments();
 
-            // if (!validatedOptions.TryGetValue("projectPath", out string _))
-            // {
-            //     Console.WriteLine("Missing argument -projectPath");
-            //     EditorApplication.Exit(110);
-            // }
-
-            // if (!validatedOptions.TryGetValue("buildTarget", out string buildTarget))
-            // {
-            //     Console.WriteLine("Missing argument -buildTarget");
-            //     EditorApplication.Exit(120);
-            // }
-
-            // if (!Enum.IsDefined(typeof(BuildTarget), buildTarget ?? string.Empty))
-            // {
-            //     Console.WriteLine($"{buildTarget} is not a defined {nameof(BuildTarget)}");
-            //     EditorApplication.Exit(121);
-            // }
-
-            // if (!validatedOptions.TryGetValue("customBuildPath", out string _))
-            // {
-            //     Console.WriteLine("Missing argument -customBuildPath");
-            //     EditorApplication.Exit(130);
-            // }
-
-            // const string defaultCustomBuildName = "TestBuild";
-            // if (!validatedOptions.TryGetValue("customBuildName", out string customBuildName))
-            // {
-            //     Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
-            //     validatedOptions.Add("customBuildName", defaultCustomBuildName);
-            // }
-            // else if (customBuildName == "")
-            // {
-            //     Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
-            //     validatedOptions.Add("customBuildName", defaultCustomBuildName);
-            // }
+            // Oh God.
 
             return validatedOptions;
         }
@@ -161,25 +84,7 @@ namespace UnityBuilderAction
             };
 
             BuildSummary buildSummary = BuildPipeline.BuildPlayer(buildPlayerOptions).summary;
-            ReportSummary(buildSummary);
             ExitWithResult(buildSummary.result);
-        }
-
-        private static void ReportSummary(BuildSummary summary)
-        {
-            Console.WriteLine(":)");
-            // Console.WriteLine(
-            //     $"{Eol}" +
-            //     $"###########################{Eol}" +
-            //     $"#      Build results      #{Eol}" +
-            //     $"###########################{Eol}" +
-            //     $"{Eol}" +
-            //     $"Duration: {summary.totalTime.ToString()}{Eol}" +
-            //     $"Warnings: {summary.totalWarnings.ToString()}{Eol}" +
-            //     $"Errors: {summary.totalErrors.ToString()}{Eol}" +
-            //     $"Size: {summary.totalSize.ToString()} bytes{Eol}" +
-            //     $"{Eol}"
-            // );
         }
 
         private static void ExitWithResult(BuildResult result)
